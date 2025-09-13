a5f8af6e4b926d5a95094d096bdf2935fccdba54
ByteSage007 <161823699+ByteSage007@users.noreply.github.com>
2025-09-13 22:10:00 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,58 +1,69 @@
+# application.py
+import os
 import pickle
-import streamlit as st
 import requests
+import streamlit as st
 
+# Try Streamlit secrets first (works on Streamlit Cloud), fallback to env var
+TMDB_API_KEY = None
+try:
+    TMDB_API_KEY = st.secrets["TMDB_API_KEY"]
+except Exception:
+    TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+
+if not TMDB_API_KEY:
+    st.warning("TMDB_API_KEY not found in st.secrets or environment variables. Poster images may fail.")
+
+@st.cache_data(show_spinner=False)  # use st.cache if your Streamlit version is older
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    if not TMDB_API_KEY:
+        return None
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={TMDB_API_KEY}&language=en-US"
+    resp = requests.get(url, timeout=5)
+    if resp.status_code != 200:
+        return None
+    data = resp.json()
+    poster_path = data.get("poster_path")
+    if not poster_path:
+        return None
+    return f"https://image.tmdb.org/t/p/w500{poster_path}"
 
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
+def recommend(movie, movies_df, similarity_matrix):
+    idx = movies_df[movies_df['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity_matrix[idx])), reverse=True, key=lambda x: x[1])
+    names, posters = [], []
     for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+        movie_id = movies_df.iloc[i[0]].movie_id
+        names.append(movies_df.iloc[i[0]].title)
+        posters.append(fetch_poster(movie_id))
+    return names, posters
 
-    return recommended_movie_names,recommended_movie_posters
+st.header("Movie Recommender System")
 
-
-st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
+# load pickles (ensure model/ exists and contains the pickles)
+movies = pickle.load(open('model/movie_list.pkl', 'rb'))
+similarity = pickle.load(open('model/similarity.pkl', 'rb'))
 
 movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
+selected_movie = st.selectbox("Type or select a movie from the dropdown", movie_list)
 
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+if st.button("Show Recommendation"):
+    recommended_names, recommended_posters = recommend(selected_movie, movies, similarity)
+    cols = st.columns(5)
+    for col, name, poster in zip(cols, recommended_names, recommended_posters):
+        with col:
+            st.write(name)
+            if poster:
+                st.image(poster, use_container_width=True)
+            else:
+                st.write("Poster not found")
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+
+
+
+
+
+
 
 
 
